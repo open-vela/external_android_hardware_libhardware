649460888156d36e507bd7adb5428ca7ba4addc9
liujinye <liujinye@xiaomi.com>
2021-10-09 00:46:52 +0800

    Merge "Delete KM1"

--- modules/sensors/dynamic_sensor/DynamicSensorManager.h
+++ modules/sensors/dynamic_sensor/DynamicSensorManager.h
@@ -111,7 +111,6 @@ private:
     // available sensor handle space
     const std::pair<int, int> mHandleRange;
     sensor_t mMetaSensor;
-    bool mMetaSensorActive = false;
 
     // immutable pointer to event callback, used in extention mode.
     SensorEventCallback * const mCallback;
