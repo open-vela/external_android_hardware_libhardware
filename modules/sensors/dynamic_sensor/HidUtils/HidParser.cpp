94deb730cd9d9459a60adda9f2870b48d83ab0da
openvela-robot <openvela-robot@xiaomi.com>
2022-02-11 18:19:22 +0800

    Merge "Add audio_hw_device.set_device_connected_state_v7 method"

--- modules/sensors/dynamic_sensor/HidUtils/HidParser.cpp
+++ modules/sensors/dynamic_sensor/HidUtils/HidParser.cpp
@@ -248,7 +248,6 @@ std::vector<HidParser::ReportPacket> HidParser::convertGroupToPacket(
                 ReportItem digest = {
                     .usage = r.getFullUsage(),
                     .id = id,
-                    .usageVector = r.getUsageVector(),
                     .minRaw = logical.first,
                     .maxRaw = logical.second,
                     .a = scale,
@@ -317,5 +316,4 @@ std::ostream& operator<<(std::ostream &os, const HidParser::DigestVector &digest
     os << LOG_ENDL;
     return os;
 }
-
 } // namespace HidUtil
