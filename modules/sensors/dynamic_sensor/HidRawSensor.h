26389d9739cf872003b0c8b9bfe4eae69fdffa7c
openvela-robot <openvela-robot@xiaomi.com>
2022-04-07 01:08:40 +0800

    Snap for 8414751 from 9b8bc0f7c1fd4565572a894d4f5380e9ae0905b9 to tm-release
    Change-Id: I2b58b7dabd2928b8080fc66cab3b049de00b22a8

--- modules/sensors/dynamic_sensor/HidRawSensor.h
+++ modules/sensors/dynamic_sensor/HidRawSensor.h
@@ -195,8 +195,6 @@ private:
     int mReportIntervalId;
     unsigned int mReportIntervalBitOffset;
     unsigned int mReportIntervalBitSize;
-    double mReportIntervalScale;
-    int64_t mReportIntervalOffset;
 
     // Input report translate table
     std::vector<ReportTranslateRecord> mTranslateTable;
